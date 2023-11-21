ddf013931c400bb2424d28db37b67c0e7b2cd5ef
botw0303 <zeldabotw0303@gmail.com>
2023-11-21 12:37:46 +0900

    BattleEffect&Skill

--- 2023_winapi_framework/Skill.h
+++ 2023_winapi_framework/Skill.h
@@ -1,5 +1,37 @@
 #pragma once
-class Skill
+#include "pokemon.h"
+enum class SkillGroup 
 {
+
+	Status,
+	Attack
+
 };
 
+enum class BattleEffectGroup 
+{
+
+	Poison,
+	Burn,
+	Paralysis,
+	Sleep,
+	Freeze,
+	Confusion,
+	Flinch,
+
+};
+
+class Skill
+{
+public:
+	Skill();
+	~Skill();
+public:
+	void BattleEffect();
+public:
+	//string _name;
+	SkillGroup _skillGroup;
+	int _damage;
+	int Accuracy;
+	PokemonType _type;
+};
