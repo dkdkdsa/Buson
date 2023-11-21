bb85797bf17608c972fb694cea8ecfe3ef595cb3
dkdkdsa <bos94330@gmail.com>
2023-11-21 10:47:22 +0900

    pokemon

--- 2023_winapi_framework/pokemon.h
+++ 2023_winapi_framework/pokemon.h
@@ -1,1 +1,28 @@
 #pragma once
+
+struct PokemonStats
+{
+
+	float Hp;
+	float Attack;
+	float Defense;
+	float Speed;
+
+};
+
+enum class PokemonType
+{
+
+	Fire,
+	Ice
+
+};
+
+struct Pokemon {
+
+	PokemonType Type;
+	PokemonStats Stats;
+
+
+};
+
