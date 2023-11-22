4263895e3872d59a516e958ce2af34577cf07d2e
dkdkdsa <bos94330@gmail.com>
2023-11-22 16:51:33 +0900

    Field

--- 2023_winapi_framework/FieldScene.cpp
+++ 2023_winapi_framework/FieldScene.cpp
@@ -8,10 +8,15 @@ void FieldScene::Init()
 
 	auto screenPoint = Core::GetInst()->GetResolution();
 
+#pragma region Field
+
 	Vec2 fieldSize = Vec2({ screenPoint.x * 0.9f, screenPoint.y / 1.5f });
 	Vec2 fieldCenter = Vec2({ screenPoint.x / 2, screenPoint.y / 2 + 50 });
 
 	auto* debug = new Field(fieldCenter, fieldSize);
 	AddObject(debug, OBJECT_GROUP::FIELD);
+
+#pragma endregion
+
 
 }
