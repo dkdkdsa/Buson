7053fa4219621fc80922d5225243884df0d60a54
dkdkdsa <bos94330@gmail.com>
2023-11-26 14:47:06 +0900

    Btn

--- 2023_winapi_framework/FieldUI.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Object.h"
-class FieldUI :
-    public Object
-{
-
-
-};
-
---
+++ 2023_winapi_framework/UIButton.cpp
@@ -0,0 +1,68 @@
+#include "pch.h"
+#include "UIButton.h"
+#include "KeyMgr.h"
+#include "ResMgr.h"
+#include "Texture.h"
+
+
+
+
+UIButton::UIButton(wstring textureKey, wstring path, Vec2 pos, Vec2 scale)
+{
+
+	_mainTex = ResMgr::GetInst()->TexLoad(textureKey, path);
+	m_vPos = pos;
+
+	if (_mainTex != nullptr) {
+
+		m_vScale = Vec2(_mainTex->GetWidth(), _mainTex->GetHeight());
+
+	}
+	else {
+
+		m_vScale = scale;
+
+	}
+
+	_btnRt = RECT_MAKE((long)pos.x, (long)pos.y, (long)scale.x, (long)scale.y);
+
+}
+
+void UIButton::Update()
+{
+
+	if (KEY_DOWN(KEY_TYPE::LBUTTON) && PtInRect(&_btnRt, KeyMgr::GetInst()->GetMousePos())) {
+
+		OnClick();
+
+	}
+
+}
+
+void UIButton::Render(HDC _dc)
+{
+
+	if (_mainTex == nullptr) {
+
+		RECT_RENDER(m_vPos.x, m_vPos.y, m_vScale.x, m_vScale.y, _dc);
+
+	}
+	else
+	{
+
+		TransparentBlt(
+			_dc,
+			m_vPos.x - m_vScale.x / 2,
+			m_vPos.y - m_vScale.y / 2,
+			m_vScale.x,
+			m_vScale.y,
+			_mainTex->GetDC(),
+			0,
+			0,
+			m_vScale.x,
+			m_vScale.y,
+			RGB(255, 0, 255));
+
+	}
+
+}
