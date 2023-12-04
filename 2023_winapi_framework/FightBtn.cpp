a2f33de70f451978261104c544f24eff31dd83d5
botw0303 <zeldabotw0303@gmail.com>
2023-12-04 13:23:06 +0900

    ready to merge

--- 2023_winapi_framework/FightBtn.cpp
+++ 2023_winapi_framework/FightBtn.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "FightBtn.h"
 
-FightBtn::FightBtn() : UIButton(L"asdf", L"asdf", Vec2({ 300, 100 }), Vec2({ 300, 75 })
+FightBtn::FightBtn() : UIButton(L"Battle_FightBtn.bmp", L"Res\\Texture\\Battle\\Button\\Battle_FightBtn.bmp", Vec2({ 300, 100 }), Vec2({ 300, 75 }))
 {
 }
 
