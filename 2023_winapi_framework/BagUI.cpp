1fcf6a8d6682693c3e7d6ec2d1419f789142d577
botw0303 <zeldabotw0303@gmail.com>
2023-12-13 17:09:05 +0900

    ready to merge

--- 2023_winapi_framework/BagUI.cpp
+++ 2023_winapi_framework/BagUI.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #include "BagUI.h"
 #include "Core.h"
-#include "FieldDeckPanel.h"
 #include "PokemonSelectBtn.h"
 BagUI::BagUI() : BattleUI(L"BattleSceneBackground.bpm", L"Texture\\Battle\\BattleSceneBackground.bpm", { 0, 0 }, { 1280, 720 })
 {
@@ -9,7 +8,7 @@ BagUI::BagUI() : BattleUI(L"BattleSceneBackground.bpm", L"Texture\\Battle\\Battl
 	auto screenPoint = Core::GetInst()->GetResolution();
 
 	for (int i = 0; i < _pokemonBtns.size(); ++i) {
-
+		SetBtnVec(_pokemonBtns[i]);
 	}
 }
 
