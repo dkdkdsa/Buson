19c56fda2d733082b8665b2d966e80ca1c29f34d
botw0303 <zeldabotw0303@gmail.com>
2023-12-16 23:42:11 +0900

    계산 공식 수정

--- 2023_winapi_framework/pokemon.cpp
+++ 2023_winapi_framework/pokemon.cpp
@@ -3,7 +3,8 @@
 #include "Skill.h"
 
 void Pokemon::UseSkill(Pokemon* enemyPokemon, Skill* usedSkill) {
-	float damage = 0.44f * (Stats.Attack / enemyPokemon->Stats.Defense); // Default Damage based on level 50
+	//float damage = 0.44f * (Stats.Attack / enemyPokemon->Stats.Defense); // Default Damage based on level 50
+	int damage = Stats.Attack * usedSkill->Damage;
 	for (int i = 0; i < Type.size(); ++i) { // Same-type attack bonus 
 		if (Type[i] == usedSkill->SkillType)
 			damage *= 1.5f;
@@ -11,5 +12,7 @@ void Pokemon::UseSkill(Pokemon* enemyPokemon, Skill* usedSkill) {
 	for (int i = 0; i < (int)PokemonType::End; ++i) {
 		damage *= TypeSheet[(int)usedSkill->SkillType][i];
 	}
+	int def = enemyPokemon->Stats.Defense * enemyPokemon->Stats.Hp / 0.411f;
+	damage = (damage / def * 44) + 2;
 	enemyPokemon->Stats.Hp -= damage;
 }
